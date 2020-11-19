c32a78c7a4ee776bce68a6b16239370e04b758ce
Jhonatan Lima <39390551+jhonyllima@users.noreply.github.com>
2020-11-19 10:53:30 -0300

    Finished component pay

--- src/components/Pay/index.js
+++ src/components/Pay/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {
+  Border,
+  Container,
+  SubContent,
+  WrapContent,
+  TextContent,
+  SubTextContent,
+} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import HeaderContentPage from '~/components/HeaderContentPage';
 
@@ -8,6 +17,30 @@ function Pay() {
   return (
     <Container>
       <HeaderContentPage />
+
+      <WrapContent>
+        <SubContent>
+          <Icon name="credit-card" size={35} color="#612f74" />
+          <TextContent>Pagar fatura do cartão</TextContent>
+          <Icon name="keyboard-arrow-right" size={35} color="#a9a9a9" />
+        </SubContent>
+        <SubTextContent>
+          Gere um boleto ou pague com a conta do Nubank
+        </SubTextContent>
+      </WrapContent>
+
+      <Border />
+
+      <WrapContent>
+        <SubContent>
+          <Icon name="qr-code" size={35} color="#612f74" />
+          <TextContent style={{marginLeft: -45}}>Pagar um boleto</TextContent>
+          <Icon name="keyboard-arrow-right" size={35} color="#a9a9a9" />
+        </SubContent>
+        <SubTextContent>
+          Gere um boleto ou pague com a conta do Nubank
+        </SubTextContent>
+      </WrapContent>
     </Container>
   );
 }
--- src/components/Pay/styles.js
+++ src/components/Pay/styles.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const WrapContent = styled.View`
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 245px;
+  margin: 15px 0;
+  padding: 0px 20px;
+`;
+
+export const TextContent = styled.Text`
+  top: -14px;
+  left: -20px;
+  font-size: 19px;
+  font-weight: bold;
+`;
+
+export const SubContent = styled.View`
+  width: 100%;
+  height: 40px;
+
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+export const SubTextContent = styled.Text`
+  top: -20px;
+
+  padding: 0 30px 0 45px;
+
+  font-size: 16px;
+`;
+export const Border = styled.View`
+  width: 100%;
+  height: 1px;
+
+  background: #c9c9c9;
+`;
